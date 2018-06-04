58a750eaaddd424578fd4c8c526a1a480b3ff4af
ReenaVerma <reena_verma@hotmail.com>
2018-06-04 22:30:55 +0100

    gatsby basic template setup

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import Link from 'gatsby-link';
 
-const Header = ({ siteTitle }) => (
+const Header = () => (
   <div
     style={{
       background: 'rebeccapurple',
-      marginBottom: '1.45rem',
+      marginBottom: '1.45rem'
     }}
   >
     <div
       style={{
         margin: '0 auto',
         maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
+        padding: '1.45rem 1.0875rem'
       }}
     >
       <h1 style={{ margin: 0 }}>
@@ -20,14 +20,14 @@ const Header = ({ siteTitle }) => (
           to="/"
           style={{
             color: 'white',
-            textDecoration: 'none',
+            textDecoration: 'none'
           }}
         >
-          {siteTitle}
+          {}
         </Link>
       </h1>
     </div>
   </div>
-)
+);
 
-export default Header
+export default Header;
