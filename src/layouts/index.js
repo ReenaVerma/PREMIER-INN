2ba6cd569e35190223ffc176acf77d7328a1d23f
ReenaVerma <reena_verma@hotmail.com>
2018-06-04 23:04:33 +0100

    functions added

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
-// import Helmet from 'react-helmet';
-
 import axios from 'axios';
 
 import Header from '../components/header';
+
 import './index.css';
 
 class Layout extends React.Component {
@@ -27,40 +25,45 @@ class Layout extends React.Component {
     console.log('COMPONENT WILL MOUNT');
   }
 
-  // FOURSQUARE COMPONENT DID MOUNT
+  // MAKE AXIOS REQUEST/COMPONENT DID MOUNT
   componentDidMount() {
     console.log('COMPONENT DID MOUNT');
 
     axios.get('https://api.foursquare.com/v2/venues/explore?near=london&client_id=JUTTZIYT3Y2ECNHCORRDKIPLW1FNSAH2PW0XRLJCMIPRKY1Q&client_secret=220HPNZNEX3I34URWK4SK33IJBA4UJM3PFSRJIFCYRJTGBBN&v=201806044')
       .then(res => {
-        console.log('data', res.data.response.groups[0].items);
+        console.log('DATA', res.data.response.groups[0].items);
         this.setState(
           { places: res.data.response.groups[0].items}
         );
       });
   }
 
+  // LISTEN TO FORM ENTRY/HANDLE CHANGE
+  handleChange(e) {
+    this.setState({ searchData: e.target.value }, () => console.log(this.state.searchData));
+  }
+
+  // CHANGE RESULTS AND APPLY SEARCH TERM TO AXIOS REQUEST
+  handleSubmit(e){
+    e.preventDefault();
+    console.log(this.state.searchData);
+    axios.get(`https://api.foursquare.com/v2/venues/explore?near=${this.state.searchData}&client_id=JUTTZIYT3Y2ECNHCORRDKIPLW1FNSAH2PW0XRLJCMIPRKY1Q&client_secret=220HPNZNEX3I34URWK4SK33IJBA4UJM3PFSRJIFCYRJTGBBN&v=201806044`)
+      .then(res => {
+        this.setState({ places: res.data.response.groups[0].items});
+        console.log(res.data.response.groups[0].items);
+      });
+  }
 
 
   render() {
     return(
-
       <div>
         <Header />
-
-
       </div>
-
-
-
     );
   }
-
 }
 
-// Layout.propTypes = {
-//   children: PropTypes.func
-// };
 
 export default Layout;
 
